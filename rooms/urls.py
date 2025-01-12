0ce421d2cd1ce8ee9280512f33c1eb21f974476b
Ndifoinhilary <ndifoinhilary@gmail.com>
2025-01-12 01:45:09 +0100

    Learned about listview and created a details function for a details of a room

--- rooms/urls.py
+++ rooms/urls.py
@@ -1,6 +1,11 @@
+from django.urls import path
 
+
+from rooms import views as room_view
 
 app_name = "rooms"
 urlpatterns = [
+
+    path("room/<int:pk>/detial/", room_view.room_details, name="room_detail"),
 
 ]
--- rooms/views.py
+++ rooms/views.py
@@ -1,8 +1,7 @@
-from math import ceil
+from django.shortcuts import get_object_or_404, render
+from django.views.generic import ListView
 
-from django.core.paginator import Paginator
-from django.shortcuts import render
-
+from rooms import models as rooms_models
 from rooms.models import Room
 
 
@@ -10,11 +9,32 @@ from rooms.models import Room
 
 
 
-def all_rooms(request):
-    page_num = request.GET.get('page', 1)
-    page = int(page_num)
-    room_list = Room.objects.all()
-    paginator = Paginator(room_list, 10, orphans=5)
-    rooms = paginator.page(page)
-    context = {'page': rooms, }
-    return render(request, 'rooms/home.html', context)
+# def all_rooms(request):
+#     page_num = request.GET.get('page', 1)
+#     page = int(page_num)
+#     room_list = Room.objects.all()
+#     paginator = Paginator(room_list, 10, orphans=5)
+#     rooms = paginator.page(page)
+#     context = {'page': rooms, }
+#     return render(request, 'rooms/room_list.html', context)
+
+
+class HomeView(ListView):
+    """
+    HomeView
+    """
+    model = rooms_models.Room
+    paginate_by = 10
+    paginate_orphans = 5
+    context_object_name = 'rooms'
+
+
+
+def room_details(request, pk):
+    room = get_object_or_404(rooms_models.Room, pk=pk)
+    amenities = room.amenities.all()
+    context = {
+        'room': room,
+        'amenities': amenities,
+    }
+    return  render(request, 'rooms/room_details.html', context)
